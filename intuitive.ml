6b969e8a091816fe313013bc6264a152bb2b621a
yro7 <yronusa2000@gmail.com>
2026-04-18 19:09:32 +0200

    feat(exercice_3): add encoder_lettre in intuitive mode

--- intuitive.ml
+++ intuitive.ml
@@ -3,3 +3,32 @@ open Chaines
 
 (* Saisie des mots en mode T9 *)
 
+
+(******************************************************************************)
+(*                                                                            *)
+(*      fonction d'encodage pour un caractère (mode intuitif)                 *)
+(*                                                                            *)
+(*   signature : encoder_lettre : encodage -> char -> int = <fun>             *)
+(*                                                                            *)
+(*   paramètre(s) : un encodage (liste associative) et un caractère           *)
+(*   résultat     : la touche correspondante                                  *)
+(*                                                                            *)
+(******************************************************************************)
+let encoder_lettre map c =
+  match List.find_map (fun (touche, lettres) ->
+    if List.mem c lettres then Some touche else None
+  ) map with
+  | Some t -> t
+  | None -> failwith "encoder_lettre : caractère non trouvé dans l'encodage"
+
+
+(* Tests unitaires *)
+
+let%test _ = encoder_lettre t9_map 'a' = 2
+let%test _ = encoder_lettre t9_map 'b' = 2
+let%test _ = encoder_lettre t9_map 'd' = 3
+let%test _ = encoder_lettre t9_map 'z' = 9
+
+let%test _ = encoder_lettre stupide_map 'a' = 2
+let%test _ = encoder_lettre stupide_map 'b' = 3
+
